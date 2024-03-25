0de7a82dbfd84469709d6f438e331ae063202399
Abdelaziz HAMMAD <azizgair7@gmail.com>
2024-03-25 13:59:45 +0100

    Gallery modified

--- src/components/gallery/Gallery.jsx
+++ src/components/gallery/Gallery.jsx
@@ -3,19 +3,22 @@ import datas from '../../../public/logements.json'
 import Card from '../card/Card'
 
 export default function Gallery() {
-
+    if (!datas || !Array.isArray(datas)) {
+        return <div>Error: Invalid data</div>;
+    }
     return (
         <main className='home_gallery'>
             {datas.map(data => {
-                return (
-                    <Card
-                        key={data.id}
-                        id={data.id}
-                        title={data.title}
-                        cover={data.cover}
-                    />
-                )
+
+                <Card
+                    key={data.id}
+                    id={data.id}
+                    title={data.title}
+                    cover={data.cover}
+                />
+
             })}
         </main>
     )
+
 }
