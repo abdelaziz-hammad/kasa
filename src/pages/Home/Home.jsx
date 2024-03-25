1d880abd07db92a0e23fa4274a8458a2e7dec5fc
Abdelaziz HAMMAD <azizgair7@gmail.com>
2024-03-25 13:53:30 +0100

    add Gallaery to the home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,12 +2,13 @@ import "./home.scss";
 import Header from "../../components/header/Header";
 import Banner from "../../components/banner/Banner";
 import Footer from "../../components/footer/Footer";
+import Gallery from "../../components/gallery/Gallery";
 export default function Home() {
 	return (
 		<div className='home'>
 			<Header />
 			<Banner />
-			
+			<Gallery />
 			<Footer />
 		</div>
 	);
